ebbb5be5d4a9129ed08c1324d3bdd0e34d51fa0c
Shwetank Chandra <96305925+Uzakun@users.noreply.github.com>
2022-03-02 06:14:50 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,27 @@
+           #Common_commands
+#pwd = returns the path of the current working directory
+
+#cd = change directory 
+  # cd .. (with two dots) to move one directory up 
+  # cd to go straight to the home folder 
+  # cd- (with a hyphen) to move to your 
+  # previous directory 
+
+#ls = view content of a directory 
+ # ls -R will list all the files in the subdirectories as well
+ # ls -a will show the hidden files
+ # ls -al will list the files and directories with detailed information like permissions, size, owner, etc.
+
+#cat = list the content of a file on the std. output 
+#  cat > filename creates a new file 
+#  cat filename1 filename2>filename3 joins two files and stores the output of them in a new file 
+
+# cp = copy files 
+
+# mv = move or rename files 
+
+# mkdir = create a new directory in the current directory 
+
+# rm = remove files and directories 
+#  rm -r to remove directory and all files inside it
+
